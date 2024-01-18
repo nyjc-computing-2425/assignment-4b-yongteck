8b07db11120f869c4d5fb0df4c29180f6aca3914
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-01-18 09:07:10 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -32,7 +32,7 @@ class TestInputOutput(unittest.TestCase):
     def check_result(self, result: str, answer: str):
         """Test the user's answer against the expected answer."""
         if answer != "":
-            self.assertNotEqual(result.strip(), "", msg=f"No output from program.")
+            self.assertNotEqual(result.strip(), "", msg="No output from program.")
         self.assertIn(result,
           answer,
           msg=f"User output {result!r} != expected output {answer!r}")
@@ -41,31 +41,31 @@ class TestInputOutput(unittest.TestCase):
         testcase = "1.1x10^-3\n"
         testans = "This number in E notation is 1.1E-3.\n"
         userans = invoke_main(testcase)
-        self.test_result(userans, testans)        
+        self.check_result(userans, testans)        
 
     def test_part2(self):
         testcase = "-1.5x10^3\n"
         testans = "This number in E notation is -1.5E3.\n"
         userans = invoke_main(testcase)
-        self.test_result(userans, testans)        
+        self.check_result(userans, testans)        
 
     def test_part3(self):
         testcase = "1.2x10^3.4\n"
         testans = "Error converting to scientific E notation.\n"
         userans = invoke_main(testcase)
-        self.test_result(userans, testans)        
+        self.check_result(userans, testans)        
 
     def test_part4(self):
         testcase = "1.O3x10^-5\n"
         testans = "Error converting to scientific E notation.\n"
         userans = invoke_main(testcase)
-        self.test_result(userans, testans)        
+        self.check_result(userans, testans)        
 
     def test_part5(self):
         testcase = "1.2x10^5.1\n"
         testans = "Error converting to scientific E notation.\n"
         userans = invoke_main(testcase)
-        self.test_result(userans, testans)        
+        self.check_result(userans, testans)        
 
 
 if __name__ == '__main__':

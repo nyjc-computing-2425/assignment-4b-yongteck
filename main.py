62ff219059024e210f0de4446c0d57426dc3c8d9
yongteck <160562805+yongteck@users.noreply.github.com>
2024-03-14 08:08:01 +0000

    final submission

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+mantissa,expo = 0,0
+validchar = "0123456789E-x.^"
+valid = len(stdform) == sum([int(i in validchar) for i in stdform])
+valid &= sum([(stdform.count(i)==1) for i in validchar[12:]]) == 3
+if valid:
+  expo = int(stdform[stdform.find("^")+1:])
+  valid &= expo//1 == expo
+  mantissa = stdform[:stdform.find("x")]
+if valid:
+  print("This number in E notation is {}E{}.".format(mantissa,expo))
+else:
+  print("Error converting to scientific E notation.")
